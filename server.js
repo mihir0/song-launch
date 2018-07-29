1009d66593eb006ec0d66096b0928b74b97a73d0
mihirparshy <mihir.parshy@hotmail.com>
2018-07-28 22:11:32 -0400

    requiring properly

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = ('express');
-const path = ('path');
+const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/song-launch'));
+// app.use(express.static('/dist/song-launch'));
 app.get('/*', (req, res) => {
-    res.sendFile(path.join('./dist/song-laungh/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/song-laungh/index.html'));
 });
 app.listen(process.env.PORT || 8080);
